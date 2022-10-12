4f7f798533d15aec19a2e62ec2496c4b68370bd0
declanketchum <60332824+declanketchum@users.noreply.github.com>
2022-10-11 20:56:48 -0400

    verilog skill part b

--- hws/4/practice.sv
+++ hws/4/practice.sv
@@ -3,4 +3,20 @@
 
 module practice(rst, clk, ena, seed, out);
 
+input wire seed, ena, rst, clk;
+output logic out;
+
+logic a, b, c, d 
+
+always_comb begin
+    a = ena ? seed : d ;
+    d = c ^ b ;
+
+    always_ff @(posedge clk) begin
+        b <= a ;
+        c <= b ; 
+        out <= c ; 
+    end
+end 
+
 endmodule
